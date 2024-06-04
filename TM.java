c4c0474e922cba4c5438681b6fc32b9c9bdc1ed8
Dennis <dennisdiener2501@gmail.com>
2024-06-04 11:39:50 +0200

    Nacharbeit fertig

---
+++ TM.java
@@ -0,0 +1,166 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+Clerk.markdown(STR."""
+# NimMC
+
+_Dennis Diener, Technische Hochschule Mittelhessen_
+
+##Disclaimer
+
+Leider kann ich aus irgendeinem Grund nicht mehr eine Instanz von TM aufrufen und dadurch nicht mehr die View programmieren. T_T
+""" );
+
+public class TM {
+    private Tape tape;
+    private Table table;
+    private String currentState;
+
+    public TM(int tapeSize, char vbz, String startState) {
+        tape = new Tape(tapeSize, vbz);
+        table = new Table();
+        currentState = startState;
+    }
+
+    public void addTransition(String fromState, char read, char write, Move move, String toState) {
+        table.addTransition(fromState, read, write, move, toState);
+    }
+
+    void step() {
+        if (currentState.equals("HALT")) {
+            return;
+        }
+        char symbol = tape.read();
+        Action action = table.getAction(currentState, symbol);
+        if (action == null) {
+            return;
+        }
+        tape.write(action.write);
+        tape.move(action.move);
+        currentState = action.State;
+    }
+
+    public void run() {
+        while (!currentState.equals("HALT")) {
+            step();
+        }
+    }
+
+    Turtle turtle = new Turtle(400, 400);
+}
+
+enum Move {
+    LEFT, RIGHT
+}
+
+class Tape {
+    private ArrayList<Character> tape;
+    private int index; //Position
+    private char vbz = ' '; //Vorbelegungszeichen = vbz
+    
+    public Tape(int size, char vbz) {   //Konstruktor des Tapes
+        tape = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            tape.add(vbz);
+        }
+        index = 0;
+        this.vbz = vbz;
+    }
+
+    //Auslesen des aktuellen Symbols
+    char read() {
+        return tape.get(index);
+    }
+
+    //Schreibe in die aktuelle Zelle/Position
+    void write(char symbol) {
+            tape.set(index, symbol);
+    }
+
+    //Bewege dich
+    void move(Move dir) { 
+        if (dir == Move.LEFT) {
+            index--;
+            if (index < 0) {
+                tape.add(0, vbz);
+                index = 0;
+            }
+        } else {
+            index++;
+            if (index >= tape.size()) {
+                tape.add(vbz);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bandString = new StringBuilder();
+        for (int i = 0; i < tape.size(); i++) {
+            if (i == index) {
+                bandString.append("#");
+            }
+            bandString.append(tape.get(i));
+            if (i == index) {
+                bandString.append("#");
+            }
+            bandString.append(" ");
+        }
+        return bandString.toString();
+    }
+}
+
+class Table {
+    private Map<Trigger, Action> transitions;
+
+    public Table() {
+        transitions = new HashMap<>();
+    }
+
+    public void addTransition(String fromState, char read, char write, Move move, String toState) {
+        transitions.put(new Trigger(fromState, read), new Action(write, move, toState));
+    }
+
+    public Action getAction(String fromState, char read) {
+        return transitions.get(new Trigger(fromState, read));
+    }
+}
+
+class Trigger {
+    String fromState;
+    char read;
+
+    Trigger(String fromState, char read) {
+        this.fromState = fromState;
+        this.read = read;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;                                    //Null abweheren!   - Nullfalschig
+        if (other == this) return true;                                     //Bin ich es selbst?- Ich ja
+        if (other.getClass() != getClass()) return false;                   //Andere Klasse?    - Klasse gleich?
+        Trigger trigger = (Trigger)other;                                   //Casting           - 
+        return read == trigger.read && fromState.equals(trigger.fromState); //Vergleich         - Was definiert Gleichheit?
+    }
+}
+
+class Action {
+    char write;
+    Move move;
+    String State;
+
+    Action(char write, Move move, String State) {
+        this.write = write;
+        this.move = move;
+        this.State = State;
+    }
+}
+
+/*   
+    Ein Array oder eine --Liste-- für das Band. -> Gemacht
+    Einen Index oder eine Position für den Lese-/Schreibekopf. -> Gemacht
+    Eine Datenstruktur für den Satz der Zustände und Übergangsregeln (z.B. HashMap oder 2D-Array).
+    Eine Schleife oder ähnliche Kontrollstruktur, um die Übergänge zu verarbeiten. 
+*/
--- abgaben/game of life/GoL.java
+++ abgaben/game of life/GoL.java
@@ -1,62 +1,154 @@
+Clerk.markdown(STR."""
+# Game of Life
+
+_Dennis Diener_, _Technische Hochschule Mittelhessen_
+
+Bei der folgenden Dokumentation werde ich kurz die Befehle für das Game of Life (GoL) erklären und die Implementierung in Java zeigen.
+
+*Vorab: Die Leinwand der Turtle wird automatisch beim erstellen von einem GoL Objekt erstellt und die View realisiert sich auch automatisch beim ausführen von der run() Methode.*
+
+In der Jshell können folgende Kommandos ausgeführt werden:
+
+Befehl | Auswirkung
+-------|-----------
+`run(int steps)`        | Führt die GoL Simulation für die angegebene Anzahl an Schritten aus
+`set(int row, int col)` | Setzt eine Zelle an die angegebenen Position
+`rotate()`              | Rotiert die GoL Welt um 90 Grad im Uhrzeigersinn
+`insert(int row, int col, GoL source)` | Fügt eine kleinere Zellenwelt in die aktuelle Spielwelt ein
+
+Die Geschwindigkeit der Animation kann man mit der Variable `speed` einstellen. Die Geschwindigkeit wird in Millisekunden angegeben.
+*Da sonst die Demo zu lange dauern würde, habe ich die Geschwindigkeit auf 10ms gesetzt.*
+""");
+
+Clerk.markdown(STR."""
+
+## Kurze Eklärung zur View
+
+Aus Zeit und Nerventechnischen Gründen habe ich die View so implementiert, dass sie sich bei einem Aufruf wie run(10) selbst erstellen soll.
+Die Leinwand wird schon beim aufruf eines GoL Objektes erstellt (wie oben schon erwähnt). Das Zeichnen geschieht dann in der run() Methode.
+     
+```java
+\{Text.cutOut("./GoL.java", "// View")}
+```
+
+""");
+
+// View
+void run(int steps){
+    for(int i=1;i<=steps;i++) {
+        startPosition(turtle); //Startposition
+        rasterVerLine(turtle); //Male die vertikalen Linien
+        rasterHorLine(turtle); //Male die horizontalen Linien
+        timestep();for(int j=0;j<world.length;j++) {
+            int rows=j/col+1;
+            int colomn=j%col+1;
+            int vertical=turtle.height/row;
+            int horizontal=turtle.width/col;
+        //System.out.println("Index: " + j + ", Row: " + rows + ", Column: " + colomn + ", Vertical: " + vertical + ", Horizontal: " + horizontal); // Testing/Debugging
+            if(world[j]==alive) {
+                turtle.penUp().forward(colomn*horizontal-(horizontal/2)).left(90).forward(rows*vertical-(vertical/2));
+                drawCell(turtle);
+                turtle.left(180).forward(rows*vertical-(vertical/2)).right(90).forward(colomn*horizontal-(horizontal/2)).right(180);
+            } try {
+                Thread.sleep(speed);
+            } catch (InterruptedException e) {
+                System.out.println("Thread wurde unterbrochen");
+                return; // Beendet die Ausführung der Methode
+            }
+        }
+    }
+}
+// View
+
+Clerk.markdown(STR."""
+### Und hier eine kleine Demo:
+
+```java
+\{Text.cutOut("./GoL.java", "// Game of Life")}
+```
+
+""");
+
 public class GoL {
     int[] world;
     int alive = 1;
     int dead = 0;
     int row; //Zeilen -> Horizontal
     int col; //Spalten -> Vertikal
+    int speed = 10; //Thread.sleep geschwindigkeit in ms
     
     //Index in der Welt berechnen -> (row-1) * max col + (col-1)!
-    Turtle turtle = new Turtle(400, 400);
+    Turtle turtle;
     
     GoL(int row, int col) {
         this.row = row;
         this.col = col;
         world = new int[row * col];
+        int tHeight = row * 100;
+        int tWidth = col * 100;
+        turtle = new Turtle(tHeight, tWidth);
     }
+    
     
     void run(int steps) {
         for (int i = 1; i <= steps; i++) {
-            turtle.reset(); //Zurücksetzen
-            turtle.penUp().forward(200).left(90).forward(200).left(90); //Oberer rechter Rand Startposition
-            rasterVerLine(turtle);
-            rasterHorLine(turtle);
+            startPosition(turtle); //Startposition
+            rasterVerLine(turtle); //Male die vertikalen Linien
+            rasterHorLine(turtle); //Male die horizontalen Linien
             timestep();
             for(int j = 0; j < world.length; j++) {
-                int row = j / col;
-                int colomn = j % col;
-                int vertical = 400 / col;
-                int horizontal = 400 / (row+1);
+                int rows = j / col+1;
+                int colomn = j % col+1;
+                int vertical = turtle.height / row;
+                int horizontal = turtle.width / col;
+                //System.out.println("Index: " + j + ", Row: " + rows + ", Column: " + colomn + ", Vertical: " + vertical + ", Horizontal: " + horizontal); // Ausgabe der Werte
                 if(world[j] == alive) {
-                    turtle.penUp().forward(colomn * vertical-(vertical/2)).left(90).forward(row * horizontal-(horizontal/2));
+                    turtle.penUp().forward(colomn * horizontal-(horizontal/2)).left(90).forward(rows * vertical-(vertical/2));
                     drawCell(turtle);
-                    turtle.left(180).forward(row * horizontal-(horizontal/2)).right(90).forward(colomn * vertical-(vertical/2)).right(180);
+                    turtle.left(180).forward(rows * vertical-(vertical/2)).right(90).forward(colomn * horizontal-(horizontal/2)).right(180);
+                }
+                try {
+                    Thread.sleep(speed);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread wurde unterbrochen");
+                    return; // Beendet die Ausführung der Methode
                 }
             }
         }
     }
-
-    private Turtle rasterVerLine(Turtle t) {
-        int vertical = 400 / col;
+    
+    void startPosition(Turtle t) {
+        turtle.reset(); //Zurücksetzen
+        turtle.penUp().forward(turtle.width/2).left(90).forward(turtle.height/2).left(90); //Startposition
+    }
+    
+    void rasterVerLine(Turtle t) {
+        int vertical = turtle.width / col;
         for (int i = 1; i < col; i++) {
-            t.penUp().forward(i * vertical).left(90).penDown().forward(400).penUp().left(180).forward(400).right(90).forward(i * vertical).right(180);
+            t.penUp().forward(i * vertical).left(90).penDown().forward(turtle.height).penUp().left(180).forward(turtle.height).right(90).forward(i * vertical).right(180);
         }
-        return t;
     }
-
-    private Turtle rasterHorLine(Turtle t) {
-        int horizontal = 400 / row;
+    
+    void rasterHorLine(Turtle t) {
+        int horizontal = turtle.height / row;
         for (int i = 1; i < row; i++) {
-            t.penUp().left(90).forward(i * horizontal).right(90).penDown().forward(400).penUp().left(180).forward(400).left(90).forward(i * horizontal).left(90);
+            t.penUp().left(90).forward(i * horizontal).right(90).penDown().forward(turtle.width).penUp().left(180).forward(turtle.width).left(90).forward(i * horizontal).left(90);
         }
-        return t;
     }
-
-    private Turtle drawCell(Turtle t) {
-        t.right(45).penDown();
-        for (int i = 0; i < 4; i++) {
-            t.forward(20).right(180).penUp().forward(20).left(90).penDown();
+    
+    void drawCell(Turtle t) {
+        int horizontal = turtle.height / row;
+        int vertical = turtle.width / col;
+        int maxRange = Math.min(horizontal, vertical) / 2 -5; //Größe der Zelle -> "Padding" zum Rand 5 px
+        t.penDown();
+        for (int i = 0; i < 360; i++) {
+            t.color(255, 255, 0); //Gelb
+            //t.color(255, 0, 255); //Rot
+            //t.color(0, 255, 255); //Blau
+            t.lineWidth(5); //Breite der Linie
+            t.forward(maxRange).left(180).forward(maxRange).left(10);
         }
-        return t.penUp().left(45);
+        t.penUp();
     }
     
     int rule(int center) {
@@ -70,27 +162,27 @@ public class GoL {
                 if (i == 0 && j == 0) continue; //Zentrum überspringen
                 int neighborRow = centerRow + i;
                 int neighborCol = centerCol + j;
-                //  Überprüfen ob Nachbarzelle existiert
+                //Überprüfen ob Nachbarzelle existiert
                 if (neighborRow >= 0 && neighborRow < row && neighborCol >= 0 && neighborCol < col) {
                     int neighborIndex = neighborRow * col + neighborCol;
-                    //  Überprüfen ob Nachbarzelle lebendig ist
+                    //Überprüfen ob Nachbarzelle lebendig ist
                     if (world[neighborIndex] == alive) {
                         count++;
                     }
                 }
             }
         }
-
+        //Regeln
         if(count == 3 && world[center] == dead) {                       //Unbelebte "center" Zelle hat exakt 3 lebende Nachbarn -> wird belebt
-            return world[center] = alive;
+        return alive;
         } else if(count <= 1 && world[center] == alive) {               //Lebende "center" Zelle hat nur eine oder keine Nachbarn -> stirbt
-            return world[center] = dead;
+        return dead;
         } else if((count == 2 || count == 3) && world[center] == alive){//Lebende "center" Zelle hat zwei oder drei Nachbarn -> bleibt am Leben
-            return world[center] = alive;
+        return alive;
         } else if(count > 3 && world[center] == alive) {                //Lebende "center" Zelle hat  mehr als drei lebende Nachbarn -> stirbt
-            return world[center] = dead;
+        return dead;
         }
-        return world[center];
+        return dead;                                                    //Ansonsten bleibt die Zelle tot
     }
 
     GoL set(int row, int col) {                             //Gezielte einsetzen von Zellen
@@ -99,9 +191,11 @@ public class GoL {
     }
 
     void timestep() {                                       //Zeitschritt
-        for(int i = 0; i < world.length; i++) {
-            rule(i);
+        int[] newWorld = new int[world.length];             //Neue Welt erstellen
+        for(int i = 0; i < world.length; i++) {             //Alle Zellen durchgehen
+            newWorld[i] = rule(i);                          //Neuen Wert berechnen und in newWorld speichern
         }
+        world = newWorld;                                   //Welt aktualisieren
     }
 
     GoL rotate() {
@@ -142,5 +236,12 @@ public class GoL {
         }
         return str;
     }
-
 }
+
+
+// Game of Life
+GoL glider = new GoL(3,3).set(1,3).set(2,3).set(3,3).set(2,1).set(3,2);
+GoL smallWorld = new GoL(20,15).insert(2,1,glider);
+glider.run(50);
+smallWorld.run(5);
+// Game of Life
