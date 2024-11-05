a6fb24e3e56f8b459783ed339dd7af42352ab250
Dennis <dennisdiener2501@gmail.com>
2024-11-05 23:37:17 +0100

    update NimMC

--- abgaben/NimMC/NimMC.java
+++ abgaben/NimMC/NimMC.java
@@ -3,21 +3,14 @@
 //
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-Clerk.markdown(STR."""
-# NimMC
+Clerk.markdown(STR.
+"""
+# Nim Turtle View
 
 _Dennis Diener, Technische Hochschule Mittelhessen_
-
-## Vorab
-Da ich in der Woche Krank war und nicht an der Vorlesung und dem Praktikum teilgenommen habe, habe ich bei einem KKommilitonen um Hilfe gebeten und wir haben uns gemeinsam
-dran gesetzt und er hat mir viel geholf und erklärt, viel davon verstehen tue ich nicht leider. Aber ich wollte mich noch einige male mit ihm zusammensetzen, damit wir an seinem
-und meinem Code arbeiten können und ich hoffentlich auholen kann.
-Deshalb wird der Code wahrscheinlich an vielen stellen gleich aussehen und gleiche Fehler haben und ich werde versuchen den Code darauf weiter aufzubauen und zu verbessern.
 
 ### Methode 1
 possibleMoves()-Methode. Diese soll alle möglichen Züge in einer eingegeben Spielsituation durchgehen und wiedergeben.
@@ -25,21 +18,7 @@ possibleMoves()-Methode. Diese soll alle möglichen Züge in einer eingegeben Sp
 ```java
 \{Text.cutOut("./NimMC.java", "// all Moves")}   
 ```
-""" );
 
-// all Moves
-public static List<Move> possibleMoves(Nim nim) { //Muss noch bearbeitet werden
-    List<Move> possibleMoves = new ArrayList<>();
-    for (int row = 0; row < nim.rows.length; row++) {
-        for (int number = 1; number <= nim.rows[row]; number++) {
-            possibleMoves.add(Move.of(row, number));
-        }
-    }
-    return possibleMoves;
-}
-// all Moves
-
-Clerk.markdown(STR."""
 ### Methode 2
 Die zweite Methode ist eine simulation(). Dort werden alle möglichen Züge eines Spiels durchgegangen und in Win und Lose gezählt.
 Danach wird in einer List MoveResults wiedergegeben, welche move, win, lose speichert für jeden einzelnen Zug.
@@ -47,119 +26,28 @@ Danach wird in einer List MoveResults wiedergegeben, welche move, win, lose spei
 ```java
 \{Text.cutOut("./NimMC.java", "// simulation")}
 ```
-""");
 
-// simulation
-public List<MoveResults> simulation(List<Move> possibleMoves) {
-    final int N = 100;
-    List<MoveResults> moveResults = new ArrayList<>();
-
-    for (Move move : possibleMoves) {
-        int win = 0;
-        int lose = 0;
-        int moveCounter = 0;
-        for (int i = 0; i < N; i++) {
-            Nim simNim = Nim.of(rows);
-            simNim = simNim.play(move);
-            moveCounter++;
-
-            boolean simWinner = NimGame.isWinning(simNim.rows);
-            if (simWinner) {
-                win++;
-            } else {
-                lose++;
-            } moveResults.add(new MoveResults(move, win, lose));
-        } return moveResults;
-    }
-}
-// simulation
-
-Clerk.markdown(STR."""
 ### Methode 3
 Des weiteren wird eine Klasse MoveResults mit den Variablen move, win, lose festgelegt.
 
 ```java
-\{Text.cutOut("./NimMC.java", "// Klasse")}
+\{Text.cutOut("./NimMC.java", "// MoveResults")}
 ```
-""");
 
-// Klasse
-class MoveResults {
-    Move move;
-    int w; //sieg
-    int l; //verloren
-
-    MoveResults(Move move, int win, int lose) {
-        this.move = move;
-        this.w = win;
-        this.l = lose;
-    }
-
-    @Override
-    public String toString() {
-        return "Move: " + move + "Win | Lose: " + w + " | " + l + " /n"; 
-    }
-}
-// Klasse
-
-Clerk.markdown(STR."""
 ### Methode 4
 mcMove soll alle vorherigen Methoden anwenden, um dann das Ergebnis der besten Moves ausrechnet und zurückgibt.
 
 ```java
 \{Text.cutOut("./NimMC.java", "// mcMove")}
 ```
+
 """);
 
-// mcMove
-public Move mcMove(){
-    assert !isGameOver();
 
-    Move mcMove;
-    double a = 0;
-
-    List<MoveResults> moveResults = simulation(possibleMoves(null));
-
-    for (MoveResults results : moveResults) {
-        double x = results.w / results.l;
-        if (x < a) {
-            x = a;
-            results.move = mcMove;
-        }
-    }
-    return mcMove;
-}
-// mcMove
+/*-------------------------------------------------------------------------------------------------------------------------------*/
 
 
-//Code
-
-class NimView extends Nim {
-    Turtle turtle = new Turtle(400, 400);
-    public NimView(int... rows) {
-        super(rows);
-    }
-
-    private Turtle holz(Turtle t) {                                                     //Streichholz zeichnen und zum nächsten Anfang gehen
-        return t.forward(20).penUp().left(90).forward(20).left(90).forward(20).left(180).penDown(); 
-    } 
-
-    private Turtle jump(Turtle t, int i) {
-        return t.penUp().right(90).forward(i * 20).left(90).forward(40).penDown();
-    }
-
-    public void show() {
-        turtle.reset(); //Zurücksetzen
-        turtle.penUp().left(90).forward(150).left(90).forward(150).left(90).penDown();//Startposition
-        for (int i = 0; i < getRows().length; i++) {
-            for (int j = 0; j < getRows()[i]; j++) {
-                holz(turtle);
-            }
-            jump(turtle, getRows()[i]); //getRows um zu schauen ob es funktioniert wenn das Spiel gespielt wird und eine neue show() zeichnen
-        }
-    }
-
-}
+// Eigentlicher Code 
 
 class Move {
     final int row, number;
@@ -191,6 +79,7 @@ interface NimGame {
     String toString();
 }
 
+// MoveResults
 class MoveResults {
     Move move;
     int w; //sieg
@@ -207,7 +96,7 @@ class MoveResults {
         return "Move: " + move + "Win | Lose: " + w + " | " + l + " /n"; 
     }
 }
-
+// MoveResults
 
 class Nim implements NimGame {
     private Random r = new Random();
@@ -215,8 +104,9 @@ class Nim implements NimGame {
     public static Nim of(int... rows) {
         return new Nim(rows);
     }
-    protected Nim(int... rows) {
-        if (rows.length > 5 || Arrays.stream(rows).anyMatch(n -> n < 0 || n > 7)) {     //Hier soll man nur eine gewisse anzahl an Streichholz"reihen" und "Streichhölzer" benutzen dürfen
+    public Nim(int ... rows) {
+        if (rows.length > 5 || Arrays.stream(rows).anyMatch(n -> n < 0 || n > 7)) {
+            //Hier soll man nur eine gewisse anzahl an Streichholz"reihen" und "Streichhölzer" benutzen dürfen
             throw new IllegalArgumentException("Ein Nim-Spiel enthält maximal fünf Reihen und jede Reihe hat maximal sieben 'Streichhölzer'.");
         }
         assert rows.length >= 1;
@@ -256,17 +146,53 @@ class Nim implements NimGame {
     public boolean isGameOver() {
         return Arrays.stream(rows).allMatch(n -> n == 0);
     }
-
     public String toString() {
         String s = "";
         for(int n : rows) s += "\n" + "I ".repeat(n);
         return s;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;                                        //Null abweheren!   - Nullfalschig
+        if (other == this) return true;                                         //Bin ich es selbst?- Ich ja
+        if (other.getClass() != getClass()) return false;                       //Andere Klasse?    - Klasse gleich?
+        Nim nim = (Nim) other;                                                  //Casten
+        return this.hashCode() == other.hashCode();                             //Vergleichen
+    }
+
+    // hashCode
+    @Override
+    public int hashCode() {
+        int sortedRows[] = new int[rows.length];                                //Kopiere das Array
+        for (int i = 0; i < rows.length; i++) {                                 
+            sortedRows[i] = rows[i];
+        }
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < sortedRows.length - 1; i++) {                   //Sortiere aufsteigend bsp. 1 2 3 4 (QuickSort)
+                if (sortedRows[i] > sortedRows[i + 1]) {
+                    int temp = sortedRows[i];
+                    sortedRows[i] = sortedRows[i + 1];
+                    sortedRows[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+        int hash = 0;                                                           //Hashwert intialisieren
+        for (int i = 0; i < sortedRows.length; i++) {                           //Berechne den Hashwert
+            hash += sortedRows[i] * Math.pow(10, i);                            //Nutze die Zehnerpotenz um die Reihenfolge zu speichern
+        }
+        return hash;
+    }
+    // hashCode
+
     public int[] getRows() {
         return rows;
     }
 
+    // mcMove
     public Move mcMove(){
         assert !isGameOver();
 
@@ -284,9 +210,10 @@ class Nim implements NimGame {
         }
         return mcMove;
     }
+    // mcMove
 
-
-    public static List<Move> possibleMoves(Nim nim) { //Muss noch bearbeitet werden
+    // all Moves
+    public static List<Move> possibleMoves(Nim nim) { 
         List<Move> possibleMoves = new ArrayList<>();
         for (int row = 0; row < nim.rows.length; row++) {
             for (int number = 1; number <= nim.rows[row]; number++) {
@@ -295,7 +222,9 @@ class Nim implements NimGame {
         }
         return possibleMoves;
     }
+    // all Moves
 
+    // simulation
     public List<MoveResults> simulation(List<Move> possibleMoves) {
         final int N = 100;
         List<MoveResults> moveResults = new ArrayList<>();
@@ -318,24 +247,40 @@ class Nim implements NimGame {
             } return moveResults;
         }
     }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == null) return false;                                        //Null abweheren!   - Nullfalschig
-        if (other == this) return true;                                         //Bin ich es selbst?- Ich ja
-        if (other.getClass() != getClass()) return false;                       //Andere Klasse?    - Klasse gleich?
-        Nim that = (Nim)other;                                                  //Casting           - 
-        return Arrays.equals(rows, that.rows);                                  //Vergleich         - Was definiert Gleichheit?
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(rows);
-    }
+    // simulation
 }
 
-//Nim nim = Nim.of(2,3,4);
-//assert nim != nim.play(Move.of(1,2)) : "Return a new Nim instance";
+// view
+class NimView extends Nim {
+    private Turtle turtle = new Turtle(400, 400);
+    
+    NimView(int... rows) {
+        super(rows);
+    }
+
+    private Turtle holz(Turtle t) {                                             //Streichholz zeichnen und zum nächsten Anfang gehen
+        return t.forward(20).penUp().left(90).forward(20).left(90).forward(20).left(180).penDown(); 
+    }
+
+    private Turtle jump(Turtle t, int i) {
+        return t.penUp().right(90).forward(i * 20).left(90).forward(40).penDown();
+    }
+
+    public void show() {                                                        //Zeichne das Spiel
+        turtle.reset();
+        turtle.penUp().left(90).forward(150).left(90).forward(150).left(90).penDown();//Startposition
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < rows[i]; j++) {
+                holz(turtle);
+            }
+            jump(turtle, rows[i]);
+        }
+    }
+}
+// view
+
+// Nim nim = Nim.of(2,3,4);
+// assert nim != nim.play(Move.of(1,2)) : "Return a new Nim instance";
 
 int[] randomSetup(int... maxN) {
     Random r = new Random();
@@ -369,3 +314,38 @@ boolean simulateGame(int... maxN) {
 assert IntStream.range(0,100).allMatch(i -> simulateGame(3,4,5));
 assert IntStream.range(0,100).allMatch(i -> simulateGame(3,4,6,8));
 
+/* // Beispielhaftes Spiel über JShell
+jshell> Nim n = Nim.of(2,3,4)
+n ==>
+I I
+I I I
+I I I I
+jshell> n = n.play(n.bestMove())
+n ==>
+I I
+I I I
+I
+jshell> n = n.play(Move.of(2,1))
+n ==>
+I I
+I I I
+jshell> n = n.play(n.bestMove())
+n ==>
+I I
+I I
+jshell> n = n.play(Move.of(1,1))
+n ==>
+I I
+I
+jshell> n = n.play(n.bestMove())
+n ==>
+I
+I
+jshell> n = n.play(Move.of(1,1))
+n ==>
+I
+jshell> n = n.play(n.bestMove())
+n ==>
+jshell> n.isGameOver()
+$25 ==> true
+*/
